b183642b7ae7708425bb99417d529c50df644905
7Bssk7 <arhip.fin3@gmail.com>
2025-12-06 22:49:49 -0800

    Update

--- 210-lab-37.cpp
+++ 210-lab-37.cpp
@@ -2,9 +2,11 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <map>
+#include <list>
 using namespace std;
 
-int sum_ascii(const string&);
+int gen_hash_index(const string&);
 
 int main() {
     char a = 'A';
@@ -14,24 +16,23 @@ int main() {
     cout << b << endl;
     cout << (char) b << endl;
 
-    int grand_sum  = 0;
+    // int grand_sum  = 0;
     string input;
     fstream in_File("lab-37-data-3.txt");
+    map<int, list<int>> hash_table;
 
     if(!in_File){
         cout << "Failed to open then file!" << endl;
         return 1;
     }
 
+    /*
     while(getline(in_File, input)){
         grand_sum += sum_ascii(input);
     }
 
     cout << "Grand total: " << grand_sum << endl;
 
-
-
-    /*
     vector<string> test {"536B9DFC93AF", "1DA9D64D02A0", "666D109AA22E", "E1D2665B21EA"};
     for (int i = 0; i < test.size(); ++i){
         cout << "Sum : " << sum_ascii(test.at(i)) << endl;
@@ -44,7 +45,7 @@ int main() {
 }
 
 
-int sum_ascii(const string & s){
+int gen_hash_index(const string & s){
     int sum = 0;
 
     for(int i = 0; i < s.size(); ++i){
